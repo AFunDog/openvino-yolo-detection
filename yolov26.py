36ec0e561cd741a4bb40d12c8ec70bd164c69ba2
Aord1 <3347313197@qq.com>
2026-06-02 16:48:01 +0800

    feat: 添加 COCO 数据集类别标签

--- yolov26.py
+++ yolov26.py
@@ -51,6 +51,22 @@ DISPLAY_BOX_THICKNESS = 2
 DISPLAY_BOX_COLOR = (0, 255, 0)
 DISPLAY_TEXT_COLOR = (0, 0, 0)
 
+# COCO 类别标签 (80类)
+COCO_CLASSES = [
+    'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck',
+    'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench',
+    'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra',
+    'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+    'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+    'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+    'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange',
+    'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+    'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse',
+    'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink',
+    'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
+    'toothbrush'
+]
+
 # UA-DETRAC 类别标签
 CLASS_NAMES = ['others', 'car', 'van', 'bus']
 
